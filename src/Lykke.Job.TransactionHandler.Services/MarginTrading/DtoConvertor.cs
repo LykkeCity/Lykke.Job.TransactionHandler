94e84960b15996ed01b177a627ae2016413cbd08
Andrey Anokhin <yashmel@mail.ru>
2017-11-17 02:40:08 +0300

    LWDEV-4011: check FinishTransferAsync result, check request to margin result, removed redundant code

---
+++ src/Lykke.Job.TransactionHandler.Services/MarginTrading/DtoConvertor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Lykke.Job.TransactionHandler.Core.Services.MarginTrading;
+using Lykke.Job.TransactionHandler.Services.Generated.MarginApi.Models;
+
+namespace Lykke.Job.TransactionHandler.Services.MarginTrading
+{
+    internal static class DtoConvertor
+    {
+        public static PaymentType ConvertToDto(this MarginPaymentType paymentType)
+        {
+            switch (paymentType)
+            {
+                case MarginPaymentType.Swift:
+                    return PaymentType.Swift;
+
+                case MarginPaymentType.Transfer:
+                    return PaymentType.Transfer;
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(paymentType), paymentType, string.Empty);
+        }
+    }
+}
