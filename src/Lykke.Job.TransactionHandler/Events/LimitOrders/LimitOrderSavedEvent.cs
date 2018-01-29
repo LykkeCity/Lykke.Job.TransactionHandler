367dc8b0fe3d47d4c482989810222b57c8c4e710
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-01-29 16:25:53 +0300

    Fixed bug with message serialization (changed type for more sense).

--- src/Lykke.Job.TransactionHandler/Events/LimitOrders/LimitOrderSavedEvent.cs
+++ src/Lykke.Job.TransactionHandler/Events/LimitOrders/LimitOrderSavedEvent.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections.Generic;
-using Lykke.Job.TransactionHandler.Core.Domain.Exchange;
 using MessagePack;
-using ProtoBuf;
 
 namespace Lykke.Job.TransactionHandler.Events.LimitOrders
 {
     [MessagePackObject(keyAsPropertyName: true)]
     public class LimitOrderSavedEvent
-    {        
-        public string Id { get; set; }        
-        public string ClientId { get; set; }        
-        public bool IsTrustedClient { get; set; }                
-        public IEnumerable<ILimitOrder> ActiveLimitOrders { get; set; }
+    {
+        public string Id { get; set; }
+        public string ClientId { get; set; }
+        public bool IsTrustedClient { get; set; }
+        public IEnumerable<string> ActiveLimitOrders { get; set; }
     }
 }
